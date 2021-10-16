dfb552897b9280238b77ce1e500e180edee6d062
Nawaz Mujawar <nawazmujawar@hotmail.com>
2021-10-16 13:45:07 +0530

    Added year filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,43 @@
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredExpenseYear, setFilteredExpenseYear] = useState("");
+
+  const filteredYearHandler = (filteredYear) => {
+    console.log(filteredYear);
+    setFilteredExpenseYear(filteredYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        date={props.expenses[0].date}
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-      />
-      <ExpenseItem
-        date={props.expenses[1].date}
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-      />
-      <ExpenseItem
-        date={props.expenses[2].date}
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-      />
-      <ExpenseItem
-        date={props.expenses[3].date}
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-      />
-    </Card>
+    <div>
+      <ExpensesFilter seletedFilteredYear={filteredYearHandler} />
+      <Card className="expenses">
+        <ExpenseItem
+          date={props.expenses[0].date}
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+        />
+        <ExpenseItem
+          date={props.expenses[1].date}
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+        />
+        <ExpenseItem
+          date={props.expenses[2].date}
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+        />
+        <ExpenseItem
+          date={props.expenses[3].date}
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+        />
+      </Card>
+    </div>
   );
 }
 
